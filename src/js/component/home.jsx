6e5f1beaf75eea5e97d1d6112c6a7dd872225449
Leonid ocampo <lnckocampo@yahoo.com>
2023-04-10 20:21:22 +0000

    cambios nuevos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
   const	[task, setTask]=useState("");
+  const	[todos, setTodos]=useState(["Tarea 1", "Tarea 2"]);
+
+  function addTask(e){
+	console.log(e.code)
+	if(e.code=="ENTER"){
+
+	}	
+  }
+
+  function deltask(index){
+
+  }
+
 	return (
 		<div className="text-center">
 			<div className="card">
@@ -15,12 +28,17 @@ const Home = () => {
 					id="exampleFormControlInput1" 
 					placeholder="Escriba tarea.."
 					value={task}	
+					onChange={(e)=>setTask(e.target.value)}
+					onKeyDown={addTask}
 					/>
 				</div>
 				<ul className="list-group list-group-flush">
-					<li className="list-group-item">An item</li>
-					<li className="list-group-item">A second item</li>
-					<li className="list-group-item">A third item</li>
+				  {
+					todos.map((todo, index)=>(<li key={index} className="list-group-item d-flex justify-content-between align-items-start">
+					{todo}
+					<button onClick={()=>deltask(index)} className="btn btn-danger btn-sm bg-danger rounded-pill">X</button>
+					</li>))
+				  }	
 				</ul>
 				<div className="card-footer">
    				 	Card footer
